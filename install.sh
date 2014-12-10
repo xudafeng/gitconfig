7ba13480bfd7a3186d0a3ac756508e24c2a4562a
xudafeng <xudafeng@126.com>
2014-12-10 13:47:18 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Install Starting …
-rm -rf ~/.gitconfig ~/.gitconfig
+rm -rf ~/.gitconfigrepo ~/.gitconfig
 git clone https://github.com/xudafeng/gitconfig.git ~/.gitconfigrepo
 ln -s ~/.gitconfigrepo/.gitconfig ~/.gitconfig
 echo Done.
